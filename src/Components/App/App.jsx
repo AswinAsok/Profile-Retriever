1477505420a045a0469cc31263cf98b8c13d7a2e
Aswin Asok <aswinasokofficial@gmail.com>
2021-04-15 07:07:53 +0530

    Project has been initalized

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,12 @@
+import Header from '../Header/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <p>This is first React Project, 2nd Try</p>
+        </div>
+    )
+}
+
+export default Header
