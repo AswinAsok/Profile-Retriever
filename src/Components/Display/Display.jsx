cc6c277413b9f0ed74883c0a0681aa8fdd0022bf
Aswin Asok <aswinasokofficial@gmail.com>
2021-04-15 13:48:10 +0530

    It is working

--- src/Components/Display/Display.jsx
+++ src/Components/Display/Display.jsx
@@ -6,10 +6,10 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import Avatar from '@material-ui/core/Avatar';
+import Avatar from "@material-ui/core/Avatar";
+import Grid from "@material-ui/core/Grid";
 
-
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     minWidth: 275,
   },
@@ -24,12 +24,16 @@ const useStyles = makeStyles({
   pos: {
     marginBottom: 12,
   },
-});
+  large: {
+    width: theme.spacing(17),
+    height: theme.spacing(17),
+  },
+}));
 
 const Display = ({ input }) => {
   const classes = useStyles();
   const [userdata, setUserdata] = useState({});
-  const [checkdata, setCheckdata] =useState(false)
+  const [checkdata, setCheckdata] = useState(false);
 
   const handleClick = (input) => {
     const username = input.input;
@@ -38,7 +42,7 @@ const Display = ({ input }) => {
       .then((res) => res.json())
       .then((data) => {
         setUserdata(data);
-        setCheckdata(true)
+        setCheckdata(true);
       });
   };
 
@@ -56,14 +60,52 @@ const Display = ({ input }) => {
         </Button>
       </div>
 
-      <Card className={classes.root} variant="outlined">
-        <CardContent>
-          <Typography variant="h6" className={classes.title}>
-            Profile Details
-          </Typography>
-        </CardContent>
-      </Card>
-      
+      <div>
+        <Card className={classes.root} variant="outlined">
+          <CardContent className="userdata">
+            <Grid
+              container
+              direction="column"
+              justify="center"
+              alignItems="center"
+            >
+              <Typography variant="h6" className={classes.title}>
+                Profile Details
+              </Typography>
+              <Avatar src={userdata.avatar_url} className={classes.large} />
+              <u>
+                <p className="username">{userdata.login}</p>
+              </u>
+              <p>{userdata.bio}</p>
+            </Grid>
+
+            <Grid
+              container
+              direction="row"
+              justify="space-around"
+              alignItems="center"
+            >
+              <b>
+                <p>
+                  {userdata.followers}
+                  <br />
+                  <br />
+                  Followers
+                </p>
+              </b>
+              <b>
+                <p>
+                  {userdata.following}
+                  <br />
+                  <br />
+                  Following
+                </p>
+              </b>
+            </Grid>
+          </CardContent>
+        </Card>
+      </div>
+    
     </div>
   );
 };
