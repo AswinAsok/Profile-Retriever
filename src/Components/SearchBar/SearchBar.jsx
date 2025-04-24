743b24a6c846c84f7ad2301343751767d679f946
Aswin Asok <aswinasokofficial@gmail.com>
2025-04-24 16:28:54 +0530

    fix-build

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,28 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import "./SearchBar.css";
 import TextField from "@material-ui/core/TextField";
 
-
 const SearchBar = ({ setInput, input }) => {
+    return (
+        <div className="searchbar-container">
+            <div className="searchbar">
+                <TextField
+                    required
+                    id="outlined-required"
+                    label="Github Username"
+                    variant="outlined"
+                    value={input}
+                    onChange={(event) => setInput(event.target.value)}
+                />
+            </div>
 
-
-  return (
-    <div className="searchbar-container">
-      <div className="searchbar">
-        <TextField
-          required
-          id="outlined-required"
-          label="Github Username"
-          variant="outlined"
-          value={input}
-          onChange={(event) => setInput(event.target.value)}
-        />
-      </div>
-
-      <br />
-
-    </div>
-  );
+            <br />
+        </div>
+    );
 };
 
 export default SearchBar;
